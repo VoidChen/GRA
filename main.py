86baf1eab582159b020a8f9ccc2c678a2ffa9355
VoidChen <void.tw@gmail.com>
2016-11-13 15:43:53 +0800

    Improve build_pfield speed

--- main.py
+++ main.py
@@ -86,11 +86,14 @@ def collision_test():
             print('collision: {}, {}'.format(*item_pair))
 
 def build_pfield(label, start, draw=False):
+    height = label.height()
+    width = label.width()
+
     #draw pfield
     def draw_pfield():
-        image = QImage(label.width(), label.height(), QImage.Format_RGB32)
-        for i in range(label.width()):
-            for j in range(label.height()):
+        image = QImage(width, height, QImage.Format_RGB32)
+        for i in range(width):
+            for j in range(height):
                 image.setPixelColor(i, j, QColor(*([255.0 * pfield[i][(j+1) * -1]/max_potential]*3)))
 
         label.clear()
@@ -102,14 +105,17 @@ def build_pfield(label, start, draw=False):
         for i in neighbor:
             nx = x+i[0]
             ny = y+i[1]
-            if label.height() > nx >= 0 and label.width() > ny >= 0 and pfield[nx][ny] is -1:
+            if height > nx >= 0 and width > ny >= 0 and pfield[nx][ny] is -1:
                 pfield[nx][ny] = pfield[x][y] + 1
                 q.put([nx, ny])
+                nonlocal max_potential
+                if pfield[nx][ny] > max_potential:
+                    max_potential = pfield[nx][ny]
 
     t = time.time()
 
     #init pfield
-    pfield = [[-1 for _ in range(label.height())] for _ in range(label.width())]
+    pfield = [[-1 for _ in range(height)] for _ in range(width)]
 
     #draw obstacle
     for item in items:
@@ -117,19 +123,16 @@ def build_pfield(label, start, draw=False):
             item.draw_pfield(pfield)
 
     #set goal
+    max_potential = 1
     pfield[int(start.x)][int(start.y)] = 0
 
-    max_potential = 1
     q = queue.Queue()
     q.put([int(start.x), int(start.y)])
     while not q.empty():
-        x, y = q.get()
-        extend(x, y)
-        if pfield[x][y] > max_potential:
-            max_potential = pfield[x][y]
+        extend(*(q.get()))
 
-    for i in range(label.width()):
-        for j in range(label.height()):
+    for i in range(width):
+        for j in range(height):
             if pfield[i][j] < 0:
                 pfield[i][j] = max_potential * 1.5
     max_potential *= 1.5
