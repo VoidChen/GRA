b2283373f8e6708094e847b2233260f584c8e966
VoidChen <void.tw@gmail.com>
2017-01-03 13:35:03 +0800

    Add straight_path

--- path.py
+++ path.py
@@ -1,9 +1,33 @@
 import copy
 import queue
 import time
+import random
 from obj import *
 from pfield import *
 from PyQt5.QtWidgets import *
+
+#validity test
+def validity_test(robot, scene, width, height):
+    #collision test
+    for obstacle in scene.obstacle:
+        if sum([(robot.init_conf[i] - obstacle.init_conf[i])**2 for i in range(2)])**0.5 <= robot.radius + obstacle.radius:
+            if Item.collision(robot, obstacle):
+                return False
+
+    for robot_init in scene.robot_init:
+        if robot_init.index != robot.index:
+            if sum([(robot.init_conf[i] - robot_init.init_conf[i])**2 for i in range(2)])**0.5 <= robot.radius + robot_init.radius:
+                if Item.collision(robot, robot_init):
+                    return False
+
+    #boundary test
+    for poly in robot.polygons:
+        for v in poly.configured(robot.conf()).vertices:
+            if not (height > v.x >= 0 and width > v.y >= 0):
+                return False
+
+    return True
+
 
 def find_path(scene, robot_index, width, height):
     def calc_pvalue(conf):
@@ -12,29 +36,6 @@ def find_path(scene, robot_index, width, height):
             temp = robot.controls[i].transform(conf)
             sum += pfields[i][int(temp.x)][int(temp.y)]
         return sum
-
-    #validity test
-    def validity_test(robot):
-        #collision test
-        for obstacle in scene.obstacle:
-            if sum([(robot.init_conf[i] - obstacle.init_conf[i])**2 for i in range(2)])**0.5 <= robot.radius + obstacle.radius:
-                if Item.collision(robot, obstacle):
-                    return False
-
-        for robot_init in scene.robot_init:
-            if robot_init.index != robot.index:
-                if sum([(robot.init_conf[i] - robot_init.init_conf[i])**2 for i in range(2)])**0.5 <= robot.radius + robot_init.radius:
-                    if Item.collision(robot, robot_init):
-                        return False
-
-        #boundary test
-        for poly in robot.polygons:
-            for v in poly.configured(robot.conf()).vertices:
-                if not (height > v.x >= 0 and width > v.y >= 0):
-                    return False
-
-        return True
-
     def extend(conf):
         if conf == goal:
             return True
@@ -44,7 +45,7 @@ def find_path(scene, robot_index, width, height):
                 new_conf = (conf[0]+i[0], conf[1]+i[1], (conf[2]+i[2])%360)
                 if new_conf not in valid and height > new_conf[0] >= 0 and width > new_conf[1] >= 0:
                     robot.init_conf = new_conf
-                    valid[new_conf] = validity_test(robot)
+                    valid[new_conf] = validity_test(robot, scene, width, height)
                     if valid[new_conf]:
                         prev_conf[new_conf] = conf
                         heap.put((calc_pvalue(new_conf), new_conf))
@@ -102,3 +103,21 @@ def find_path(scene, robot_index, width, height):
     else:
         print('Fail...')
         return [init], valid
+
+def straight_path(start, end):
+    diff = [e-s for s, e in zip(start, end)]
+    absdiff = [x if x > 0 else -x for x in diff]
+    steps = []
+    for i in range(3):
+        sign = 1 if diff[i] > 0 else -1
+        steps += [(i, sign) for _ in range(absdiff[i])]
+
+    random.shuffle(steps)
+
+    result = [start[:]]
+    current = start
+    for step in steps:
+        current[step[0]] += step[1]
+        result.append(current[:])
+
+    return result
