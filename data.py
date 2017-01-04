a81786a23cdfa9717de3f4afb38bd503b9f13087
VoidChen <void.tw@gmail.com>
2017-01-04 21:14:35 +0800

    Add load data error handle

--- data.py
+++ data.py
@@ -8,19 +8,27 @@ def read_robot(scene, filename):
             if not line.startswith('#'):
                 data += line.split()
 
-    scene.robot_num = int(data.pop(0))
+    try:
+        scene.robot_num = int(data.pop(0))
 
-    scene.robot_init = []
-    for i in range(scene.robot_num):
-        scene.robot_init.append(Robot(data))
-        scene.robot_init[-1].index = i
+        scene.robot_init = []
+        for i in range(scene.robot_num):
+            scene.robot_init.append(Robot(data))
+            scene.robot_init[-1].index = i
 
-    scene.robot_goal = []
-    for robot in scene.robot_init:
-        scene.robot_goal.append(copy.deepcopy(robot))
-        scene.robot_goal[-1].init_conf = scene.robot_goal[-1].goal_conf
+        scene.robot_goal = []
+        for robot in scene.robot_init:
+            scene.robot_goal.append(copy.deepcopy(robot))
+            scene.robot_goal[-1].init_conf = scene.robot_goal[-1].goal_conf
 
-    scene.items = [scene.robot_init, scene.robot_goal, scene.obstacle]
+        scene.items = [scene.robot_init, scene.robot_goal, scene.obstacle]
+
+    except:
+        print('Load {} fail'.format(filename))
+        scene.robot_num = 0
+        scene.robot_init = []
+        scene.robot_goal = []
+        scene.items = [scene.robot_init, scene.robot_goal, scene.obstacle]
 
 def read_obstacle(scene, filename):
     data = []
@@ -29,10 +37,17 @@ def read_obstacle(scene, filename):
             if not line.startswith('#'):
                 data += line.split()
 
-    scene.obstacle_num = int(data.pop(0))
-    scene.obstacle = []
-    for i in range(scene.obstacle_num):
-        scene.obstacle.append(Obstacle(data))
-        scene.obstacle[-1].index = i
+    try:
+        scene.obstacle_num = int(data.pop(0))
+        scene.obstacle = []
+        for i in range(scene.obstacle_num):
+            scene.obstacle.append(Obstacle(data))
+            scene.obstacle[-1].index = i
 
-    scene.items = [scene.robot_init, scene.robot_goal, scene.obstacle]
+        scene.items = [scene.robot_init, scene.robot_goal, scene.obstacle]
+
+    except:
+        print('Load {} fail'.format(filename))
+        scene.obstacle_num = 0
+        scene.obstacle = []
+        scene.items = [scene.robot_init, scene.robot_goal, scene.obstacle]
